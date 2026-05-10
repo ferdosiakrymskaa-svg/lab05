2d7ad6a9e3e7783e272525e852fd92673de29e6d
ferdosiakrymskaa-svg <ferdosiakrymskaa@gmail.com>
2026-05-10 11:09:32 +0000

    jhj

---
+++ build/third-party/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ilyasov_ilya/ferdosiakrymskaa-svg/workspace/projects/lab05/lab05/third-party/gtest/googletest
+# Build directory: /home/ilyasov_ilya/ferdosiakrymskaa-svg/workspace/projects/lab05/lab05/build/third-party/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
